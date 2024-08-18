babbb329c630f4cbd68c1cd80e05a6362e406814
Alex Coleman <hello@alexjcoleman.me>
2024-08-18 19:47:36 +0100

    Use beautifulsoup4 in IT page object

--- tests/integration/test_start.py
+++ tests/integration/test_start.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
 
+from bs4 import BeautifulSoup
 from flask.testing import FlaskClient
 from werkzeug.test import TestResponse
 
 
 class StartPage:
     def __init__(self, response: TestResponse):
-        self._response = response
-        self.is_visible = "<h2>Welcome to Wroblewshop</h2>" in self._response.text
+        self._soup = BeautifulSoup(response.text, "html.parser")
+        heading = self._soup.select_one("h2")
+        assert heading
+        self.is_visible = (
+            heading.string == "Welcome to Wroblewshop" if heading.string else False
+        )
 
     @classmethod
     def open(cls, client: FlaskClient) -> StartPage:
