707fd8bdcbab49a3afff921eb8d0c32e84f31373
Alex Coleman <hello@alexjcoleman.me>
2024-09-22 20:39:46 +0100

    Extract IT page objects

--- tests/integration/test_home.py
+++ tests/integration/test_home.py
@@ -1,21 +1,8 @@
 from __future__ import annotations
 
-from bs4 import BeautifulSoup
 from flask.testing import FlaskClient
-from werkzeug.test import TestResponse
 
-
-class HomePage:
-    def __init__(self, response: TestResponse):
-        self._soup = BeautifulSoup(response.text, "html.parser")
-        heading = self._soup.select_one("h2")
-        assert heading
-        self.is_visible = heading.string == "Home" if heading.string else False
-
-    @classmethod
-    def open(cls, client: FlaskClient) -> HomePage:
-        response = client.get("/home")
-        return HomePage(response)
+from tests.integration.pages import HomePage
 
 
 def test_home_shows_heading(client: FlaskClient) -> None:
--- tests/integration/test_start.py
+++ tests/integration/test_start.py
@@ -1,31 +1,8 @@
 from __future__ import annotations
 
-from bs4 import BeautifulSoup, Tag
 from flask.testing import FlaskClient
-from werkzeug.test import TestResponse
 
-
-class StartPage:
-    def __init__(self, response: TestResponse):
-        self._soup = BeautifulSoup(response.text, "html.parser")
-        navbar = self._soup.select_one("header.navbar")
-        assert navbar
-        self.navbar = NavbarComponent(navbar)
-        heading = self._soup.select_one("h2")
-        assert heading
-        self.is_visible = heading.string == "Welcome to Wroblewshop" if heading.string else False
-
-    @classmethod
-    def open(cls, client: FlaskClient) -> StartPage:
-        response = client.get("/")
-        return StartPage(response)
-
-
-class NavbarComponent:
-    def __init__(self, navbar: Tag):
-        navbar_link = navbar.select_one("a")
-        assert navbar_link
-        self.link = navbar_link["href"]
+from tests.integration.pages import StartPage
 
 
 def test_start_shows_heading(client: FlaskClient) -> None:
