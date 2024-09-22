66a13fdce7cf90774e22a6140bfbe1ebf06e83ea
Alex Coleman <hello@alexjcoleman.me>
2024-09-22 20:27:14 +0100

    Add IT for navbar link to start

--- tests/integration/test_start.py
+++ tests/integration/test_start.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from flask.testing import FlaskClient
 from werkzeug.test import TestResponse
 
@@ -8,6 +8,9 @@ from werkzeug.test import TestResponse
 class StartPage:
     def __init__(self, response: TestResponse):
         self._soup = BeautifulSoup(response.text, "html.parser")
+        navbar = self._soup.select_one("header.navbar")
+        assert navbar
+        self.navbar = NavbarComponent(navbar)
         heading = self._soup.select_one("h2")
         assert heading
         self.is_visible = heading.string == "Welcome to Wroblewshop" if heading.string else False
@@ -18,10 +21,23 @@ class StartPage:
         return StartPage(response)
 
 
+class NavbarComponent:
+    def __init__(self, navbar: Tag):
+        navbar_link = navbar.select_one("a")
+        assert navbar_link
+        self.link = navbar_link["href"]
+
+
 def test_start_shows_heading(client: FlaskClient) -> None:
     start_page = StartPage.open(client)
 
     assert start_page.is_visible
+
+
+def test_start_nav_shows_start(client: FlaskClient) -> None:
+    start_page = StartPage.open(client)
+
+    assert start_page.navbar.link == "/"
 
 
 def test_start_shows_home_when_logged_in(client: FlaskClient) -> None:
