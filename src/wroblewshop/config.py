89279c97758ff7dae69ba238085761705057f915
Alex Coleman <hello@alexjcoleman.me>
2024-11-19 21:19:28 +0000

    Bring up Google OAuth config
    Allows app to be run locally against Google OAuth.

--- src/wroblewshop/config.py
+++ src/wroblewshop/config.py
@@ -10,13 +10,13 @@ class Config:
     SESSION_TYPE = "sqlalchemy"
     PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
 
+    GOOGLE_OAUTH_CLIENT_ID = "854325940723-clpnbi3kjqm1khs759hb1lt7ls8ve84g.apps.googleusercontent.com"
+    GOOGLE_SERVER_METADATA_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
 class LocalConfig(Config):
     NAME = "local"
 
     TESTING = True
 
-
 class FlyConfig(Config):
-    GOOGLE_OAUTH_CLIENT_ID = "854325940723-clpnbi3kjqm1khs759hb1lt7ls8ve84g.apps.googleusercontent.com"
-    GOOGLE_SERVER_METADATA_URL = "https://accounts.google.com/.well-known/openid-configuration"
+    pass
