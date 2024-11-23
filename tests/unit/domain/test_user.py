7938a044fef74f821919af6e058798f2e4f1d2fa
Alex Coleman <hello@alexjcoleman.me>
2024-11-23 19:14:22 +0000

    Use test class for User domain

--- tests/unit/domain/test_user.py
+++ tests/unit/domain/test_user.py
@@ -1,7 +1,8 @@
 from wroblewshop.domain.user import User
 
 
-def test_user_create() -> None:
-    user = User(email="shopper@gmail.com", cupboard="Palace")
+class TestUser:
+    def test_create(self) -> None:
+        user = User(email="shopper@gmail.com", cupboard="Palace")
 
-    assert user.email == "shopper@gmail.com" and user.cupboard == "Palace"
+        assert user.email == "shopper@gmail.com" and user.cupboard == "Palace"
